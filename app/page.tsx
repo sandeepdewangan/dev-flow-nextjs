4ab1f1235cee424d70a34bf6d48a55933b60f21c
Sandeep <sandeepdewangan7@gmail.com>
2025-04-05 11:28:19 +0530

    Shadcn Setup and Example

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl text-primary-500">Hello world!</h1>
-      <h1 className="text-3xl text-primary-500 font-space-grotesk">
-        Hello world!
-      </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
