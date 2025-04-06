aa1aba001064bd47e7de33139a0031e294133ee7
Sandeep <sandeepdewangan7@gmail.com>
2025-04-06 06:55:21 +0530

    Design

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="flex flex-col items-center">
+      <span className="text-2xl">DevFlow</span>
+      <div className="pt-5">{children}</div>
+      <div className="flex gap-2 pt-5">
+        <Button>Login with Github</Button>
+        <Button>Login with Google</Button>
+      </div>
+    </main>
+  );
 };
 
 export default AuthLayout;
--- app/(auth)/sign-out/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignOut = () => {
-  return <div>Sign out</div>;
-};
-
-export default SignOut;
